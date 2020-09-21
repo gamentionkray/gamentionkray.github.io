823ec1923f8b98611c355f64b1868eb7c960413d
Krishna Thorat <krishnathorat007@gmail.com>
2020-09-21 23:14:36 +0530

    Updates

---
+++ precache-manifest.bbf10cb43663f9ce0f608e21aff81468.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9801fd015b4a78d3e88e6c8c7dae2bf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d44a9af5be5ab466bd20",
+    "url": "/static/css/2.57db6315.chunk.css"
+  },
+  {
+    "revision": "66b9fdecab5c6a7bcfdb",
+    "url": "/static/css/main.ebf0d1bd.chunk.css"
+  },
+  {
+    "revision": "d44a9af5be5ab466bd20",
+    "url": "/static/js/2.c52b4b49.chunk.js"
+  },
+  {
+    "revision": "99fd3f7591fb649b69adce4d874c85e5",
+    "url": "/static/js/2.c52b4b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b9fdecab5c6a7bcfdb",
+    "url": "/static/js/main.41cc1672.chunk.js"
+  },
+  {
+    "revision": "2b95c21d26e48c157587",
+    "url": "/static/js/runtime-main.ca7d0b2b.js"
+  },
+  {
+    "revision": "3df812f7ce490ffc06c57c8410fd70f7",
+    "url": "/static/media/krishna.3df812f7.jpg"
+  },
+  {
+    "revision": "e614e75269c032ad63694fc042723723",
+    "url": "/static/media/noimg.e614e752.png"
+  }
+]);
