17b2eb85416ef0cfd95f5adf29dda720c94a3ae8
Krishna Thorat <krishnathorat007@gmail.com>
2023-04-17 23:48:03 +0530

    update

--- api/disableWebiste.php
+++ api/disableWebiste.php
@@ -1,9 +1,10 @@
 <?php
 header('Content-Type: application/json');
-
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 
-$website_disabled = false;
+$website_disabled = true;
+$html = "<div style='min-height: 100vh; display: flex; justify-content: center; align-items: center;'><h1>Webiste Under Attack!</h1></div>";
 
-echo json_encode(array("disabled" => $website_disabled));
+echo json_encode(array("disabled" => $website_disabled, "html" => $html));
+exit();
